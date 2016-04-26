bdc41452603d9e74478655f2ce39a5df82457d13
Yahiaoui Rachid <rac021@hotmail.com>
2016-04-26 15:49:07 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 docker run -d --net mynet123 --name host_2 --add-host host_0:192.168.56.100 --add-host host_1:192.168.56.101 --add-host host_2:192.168.56.102 --ip 192.168.56.102 -p 7777:9999 --expose 9999 -it --entrypoint /bin/bash blazegraph -c "./bigdata start ; while true; do sleep 1000; done "
 docker run -d --net mynet123 --name host_1 --add-host host_0:192.168.56.100 --add-host host_1:192.168.56.101 --add-host host_2:192.168.56.102 --ip 192.168.56.101 -p 8888:9999 -it --entrypoint /bin/bash blazegraph -c "./bigdata start; while true; do sleep 1000; done "
